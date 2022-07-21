9aa673d51e41a63321986bc78500b88d0a148003
Morad Mouhttouch <moradmouhttouch@gmail.com>
2022-07-21 17:56:35 +0000

    added Raise error as well as function to convet str to li

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+SHEET = GSPREAD_CLIENT.open('Cope-spread-sheet')
 
 
 def get_sales_data():
@@ -22,7 +22,26 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
 
+
+def validate_data(values):
+    """
+     Converts all strings values into ints , 
+     Raise ValueError if stringscannot be converted into int,
+     or if there arent exactly 6 values
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f'Exactly 6 values reqiured , you provided {len(values)}'
+            )
+
+    except ValueError as e:
+        print(f"invalid data: {e} , pleas tru agian. \n ")
+    
+    print(values)
 
 get_sales_data()
