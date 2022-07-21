5ff6cd78adab90d822ddce799fb97ab2914a73ad
Morad Mouhttouch <moradmouhttouch@gmail.com>
2022-07-21 19:30:38 +0000

    added while loop ot loop throw the Raise error if worong input is inserted

--- run.py
+++ run.py
@@ -17,14 +17,20 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
     
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
+
+        if validate_data(sales_data):
+            print("data is valid!")
+            break
+    return sales_data
 
 
 def validate_data(values):
@@ -43,5 +49,9 @@ def validate_data(values):
 
     except ValueError as e:
         print(f"invalid data: {e} , pleas try agian. \n ")
+        return False
 
-get_sales_data()
+    return True
+
+data = get_sales_data()
+
